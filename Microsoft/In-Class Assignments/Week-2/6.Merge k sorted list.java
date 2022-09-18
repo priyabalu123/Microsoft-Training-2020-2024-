da8d0e074033b57cfb90d5b9bfe513550c5c5026
priyabalu123 <108317247+priyabalu123@users.noreply.github.com>
2022-09-18 11:50:43 +0530

    Update 6.Merge k sorted list.java

--- Microsoft/In-Class Assignments/Week-2/6.Merge k sorted list.java
+++ Microsoft/In-Class Assignments/Week-2/6.Merge k sorted list.java
@@ -1,37 +1,27 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0)
+        if(lists.length==0){
             return null;
-        int interval=1;
-        while(interval<lists.length)
-        {
-            system.out.println(lists.length);
-            for(int i=0;i+interval<lists.length;i=i+interval*2)
-            {
-                lists[i]=mergeTwoLists(list[i],lists[i+interval]);
-            }
-            interval*=2;
         }
-        return list[0];
+        PriorityQueue<ListNode> pq = new PriorityQueue<ListNode>(lists.length, (a,b)-> a.val-b.val);
+        for(ListNode node:lists){
+            if(node!=null){
+                pq.add(node);
+            }
+        }
+        ListNode dummy = new ListNode(0);
+        ListNode head = dummy;
+        while(!pq.isEmpty()){
+            ListNode node = pq.poll();
+            if(node.next!=null){
+                pq.add(node.next);
+            }
+            dummy.next = node;
+            dummy = node;
+        }
+        return head.next;
     }
 }
-public ListNode mergeTwoLists(ListNode l1,ListNode l2)
-{
-    if(l1==null)
-        return l2;
-    if(l2==null)
-        return l1;
-    if(l1.val<l2.val)
-    {
-        l1.next=mergeTwoLists(l1.next,l2);
-        return l1;
-    }
-    else
-    {
-        l2.next=mergeTwoLists(l1,l2.next);
-        return l2;
-    }
-} 
 /*Input: lists = [[1,4,5],[1,3,4],[2,6]]
 Output: [1,1,2,3,4,4,5,6]
 Explanation: The linked-lists are:
