1b2e09f5d5f0186313e3831af0573a65b5eda6b8
priyabalu123 <108317247+priyabalu123@users.noreply.github.com>
2022-09-18 10:35:27 +0530

    Update 1.Two Sum.java

--- Microsoft/In-Class Assignments/Week-1/1.Two Sum.java
+++ Microsoft/In-Class Assignments/Week-1/1.Two Sum.java
@@ -1,1 +1,17 @@
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans=new int[2];
+        HashMap<Integer,Integer>Map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(Map.containsKey(target-nums[i])){
+                ans[0]=Map.get(target-nums[i]);
+                ans[1]=i;
+                return ans;
+            }
+            else{
+                Map.put(nums[i],i);
+            }
+        }
+        return ans;
+    }
+}
