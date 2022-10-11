4e775e5d58ee05931a6fb897edb347be189d2d0b
priyabalu123 <108317247+priyabalu123@users.noreply.github.com>
2022-10-11 12:25:04 +0530

    Create 02.first Missing positive.java

---
+++ Microsoft/Homework Assignments/Week-1/02.first Missing positive.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode reverseBetween(ListNode head, int m, int n) { 
+        ListNode preRev = null;
+        ListNode curr = head;
+        int i = 1;
+        while(i<m) { preRev = curr; curr = curr.next; i++; }
+        ListNode end = curr;     
+        ListNode pre = null; 
+        while(i<=n) {
+         ListNode temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;
+            i++;
+        }
+        end.next = curr;
+        if(preRev != null) preRev.next = pre;
+        return preRev == null ? pre: head;    
+    }
+}
