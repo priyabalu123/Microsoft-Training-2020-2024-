7f26c35cb449d8c8d4fbac096c26cddafa6d6bae
priyabalu123 <108317247+priyabalu123@users.noreply.github.com>
2022-10-11 15:27:43 +0530

    Create 04.Find Median from Data Stream.java

---
+++ Microsoft/In-Class Assignments/Week-7/04.Find Median from Data Stream.java
@@ -0,0 +1,31 @@
+class MedianFinder {
+
+    private Queue<Long> small = new PriorityQueue(),
+                        large = new PriorityQueue();
+
+    public void addNum(int num) {
+        large.add((long) num);
+        small.add(-large.poll());
+        if (large.size() < small.size())
+            large.add(-small.poll());
+    }
+
+    public double findMedian() {
+        return large.size() > small.size()
+               ? large.peek()
+               : (large.peek() - small.peek()) / 2.0;
+    }
+};
+/*Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+
+Explanation
+MedianFinder medianFinder = new MedianFinder();
+medianFinder.addNum(1);    // arr = [1]
+medianFinder.addNum(2);    // arr = [1, 2]
+medianFinder.findMedian(); // return 1.5 (i.e., (1 + 2) / 2)
+medianFinder.addNum(3);    // arr[1, 2, 3]
+medianFinder.findMedian(); // return 2.0*/
